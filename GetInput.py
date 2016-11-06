65199f5196521dd7c01ce16056940c095ad1db70
leonzucchini <leon.zucchini@gmail.com>
2016-11-06 14:50:00 +0100

    Cleaning up
    Got main code working to the point of downloading the first pages of
    the categories. Fixed errors and flags.

--- GetInput.py
+++ GetInput.py
@@ -3,62 +3,61 @@ import os
 import sys
 
 class UserInput(object):
-        """Retrieve user input.
-        Currently assumes input files are json format.
+    """Retrieve user input.
+    Currently assumes input files are json format.
+    """
+    def __init__(self):
+        self.patherror = ""
+        self.filepath = ""
+        self.json = ""
+        self.folderpath = ""
+        self.filepaths = ""
+        self.filenames = ""
+
+    def path_exists(self, path):
+        """Check a path exists"""
+
+        if not os.path.exists(path):
+            self.patherror = 'GetInput error: Input path does not exist \n%s' %(path)
+            print self.patherror
+            sys.exit(1)
+        else:
+            pass
+        return self
+
+    def get_dict(self, filepath):
+        """Retrieve user input from preference / other files.
+        Assumes input files are json format.
+        Attribute defined: Dictionary of input
         """
+        self.filepath = filepath
+        self.path_exists(self.filepath)
 
-        def __init__(self):
-            self.patherror = ""
+        with open(filepath, "r") as f:
+            self.json = json.load(f)
+        return self
 
-        def path_exists(self, path):
-            """Check a path exists"""
+    ### This would actually be nicer to do differently
+    ### because it currently only has one attribute slot
+    ### for input (but don't want to waste time fiddling)
+    ### around with setattr() now.
 
-            if not os.path.exists(path):
-                self.patherror = \
-                    'GetInput error: Input path does not exist \n%s' %(path)
-                print self.patherror
-                sys.exit(1)
-            else:
-                pass
-            return self
-
-        def getDict(self, filepath):
-            """Retrieve user input from preference / other files.
-            Assumes input files are json format.
-            Attribute defined: Dictionary of input
-            """
-            self.filepath = filepath
-            self.path_exists(self.filepath)
-
-            with open(filepath, "r") as f:
-                self.json = json.load(f)
-            return self
-
-            ### This would actually be nicer to do differently
-            ### because it currently only has one attribute slot
-            ### for input (but don't want to waste time fiddling)
-            ### around with setattr() now.
-
-
-        def getFilePaths(self, folderpath):
-            """Retrieve files containing user input from folder.
-            Attribute defined: List of input file paths based on folder path.
-            """
-            # Get list of filenames from folder
-            self.folderpath = folderpath
-
-            self.path_exists(self.folderpath)
-            self.filenames = os.listdir(folderpath)
+    def getFilePaths(self, folderpath):
+        """Retrieve files containing user input from folder.
+        Attribute defined: Diectionary of input file paths based on folder path.
+        """
+        # Get list of filenames from folder
+        self.folderpath = folderpath
+        self.path_exists(self.folderpath)
+        self.filenames = os.listdir(folderpath)
             
-            # Construct file paths
-            self.filepaths = []
-            for name in self.filenames:
-                self.filepaths.append(
-                  os.path.join(self.folderpath, name)  
-                )
-            return self
+        # Construct file paths
+        self.filepaths = {}
+        for name in self.filenames:
+            self.filepaths[name] = os.path.join(self.folderpath, name) 
+        return self
 
-        def print_prefs(self, attr):
-            """ Print json file nicely for a given json attribute."""
-            attr = getattr(self, attr)
-            print json.dumps(attr, indent=4, sort_keys=True)
+    def print_prefs(self, attr):
+        """ Print json file nicely for a given json attribute."""
+        attr = getattr(self, attr)
+        print json.dumps(attr, indent=4, sort_keys=True)
--- WriteObject.py
+++ WriteObject.py
@@ -3,27 +3,16 @@ import sys
 import re
 import shutil
 
-class WriteObject:
-    def __init__(self, folder):
-        """Save string to .txt file in a specified location under a specified name.
-        
-        Arguments:
-        --folder: Folder path where the result should be stored. Relative to cwd.
-        --debug: If true then skip check whether results folder exists. 
-        """
-        self.dstfolder = os.path.join(os.getcwd(), folder)
-
-    def write_file(self, input_text, file_name):
+class WriteObject(object):
+    def __init__(self, inputtext, filepath):
         """Write file to disk, exiting if it already exists."""
 
-        self.input_text = input_text
-        self.file_name = file_name
-        self.dstpath = os.path.join(self.dstfolder, self.file_name)
+        self.inputtext = inputtext
+        self.filepath = filepath
 
-        # Check whether the file already exists, exit if yes, else write file
-        if os.path.exists(self.dstpath):
-            print "Something is wrong: This file already exists. Exiting so you can fix stuff."
+        if os.path.exists(self.filepath):
+            print "WriteObject error: This file already exists. Exiting..."
             sys.exit(1)
         else:
-            with open(self.dstpath, 'w') as f:
-                f.write(self.input_text.encode('utf8'))
+            with open(self.filepath, 'w') as f:
+                f.write(self.inputtext)
