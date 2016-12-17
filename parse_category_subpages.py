f2f2d5e5c6a319608088034e7aa871f71771fab0
leonzucchini <leon.zucchini@gmail.com>
2016-12-17 19:35:07 +0100

    Fixed parsing (re) for link info

--- parse_category_subpages.py
+++ parse_category_subpages.py
@@ -6,24 +6,31 @@ from bs4 import BeautifulSoup as bs
 import pandas as pd
 # import py2neo as pn
 
-def parse_info(file_path):
+def parse_info(file_path, issue_counter=1):
     """ Grab html code from a file and parse for information - return dict of dicts. """
 
+    issue_counter = issue_counter
+    # Open file and parse with BS4
     with open(file_path, "r") as f:
         soup = bs(f.read().decode("utf-8"), "lxml")
-
+    
+    # Get category
     category = re.match(r"g\d+(\w+)_.*", os.path.basename(file_path)).group(1)
     recipes = {}
 
     search_hits = soup.find_all("li", class_="search-list-item")
-    
-    issue_counter = 1
     for hit in search_hits:
     # Parse results in search hit for basic information
+
         try:
             # Recipe ID
             id = hit['id'].replace('recipe-','')
             recipes[id] = {}
+
+            # Info on category and category list page
+            recipes[id]["category"] = category
+            pNum, rText, sNum = re.match(r".*(g\d+)(.*)_(\d*).*\.(txt)", file_path).group(1,2,3)
+            recipes[id]["category_list_page"] = "www.chefkoch.de/rs/s" + sNum + pNum + "/" + rText + ".html"
 
             # URL, title, subtitle
             recipes[id]["url"] = "".join(["www.chefkoch.de", hit.a['href']])
@@ -32,16 +39,20 @@ def parse_info(file_path):
 
             # Votes (number and average)
             votes_raw = hit.a.find("span", class_="search-list-item-uservotes-stars")["title"]
-            recipes[id]["votes_n"] = re.match(r"(\d*)\s.*?(\d+\.\d+)", votes_raw).group(1)
-            recipes[id]["votes_avg"] = re.match(r"(\d*)\s.*?(\d+\.\d+)", votes_raw).group(2)
+            recipes[id]["votes_n"] = re.match(r"^(\d*)\s.*", votes_raw).group(1)
+            recipes[id]["votes_avg"] = re.match(r".*\s.*([\d*\.]$)", votes_raw).group(1)
 
             # Other info
             recipes[id]["difficulty"] = hit.a.find("span", class_="search-list-item-difficulty").get_text()
             recipes[id]["preptime"] = hit.a.find("span", class_="search-list-item-preptime").get_text()
             recipes[id]["activationdate"] = hit.a.find("span", class_="search-list-item-activationdate").get_text()
+
+            print file_path + " #" + str(issue_counter) + " successfully parsed"
+
         except Exception:
-            print "Problem with parsing #" + str(issue_counter)
-            issue_counter += 1
+            print file_path + " #" + str(issue_counter) + " problem with parsing"
+        
+        issue_counter += 1
 
     return recipes
 
@@ -57,16 +68,19 @@ def main():
     file_paths = []
     [file_paths.append(os.path.join(folder_path, fn)) for fn in os.listdir(folder_path)]
     file_paths.pop(0) # Remove log file from list
+    # file_paths = "/Users/Leon/Documents/02_Research_Learning/Research/Recipes/03_Data/textFiles/g102Getraenke_1020.txt"
 
-    # Parse information by looping over files
-    # Store to pandas dict and write info for first analysis (w/o neo2j database)
+    # # Parse information by looping over files
+    # # Store to pandas dict and write info for first analysis (w/o neo2j database)
     link_data = pd.DataFrame()
-    for fp in file_paths:
-        page_hits = parse_info(fp)
+    issue_counter = 1
+    for fp in file_paths[:100]:
+        page_hits = parse_info(fp, issue_counter=issue_counter)
+
         for k, v in page_hits.items():
             row = pd.Series(v, name=k)
             link_data = link_data.append(row)
-        
+
     link_data.to_csv(out_path, encoding='utf8')
 
 main()
